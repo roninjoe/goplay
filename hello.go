9b17bb5ffa5fc454e012eafae03e8c8a79b69446
joe <3817306+roninjoe@users.noreply.github.com>
2020-08-10 16:48:05 -0400

    add some "OO"ish

--- hello.go
+++ hello.go
@@ -6,6 +6,25 @@ import (
 	"example.com/user/hello/morestrings"
 )
 
+// An animal object (sort of)
+type animal struct {
+	name string
+	age  int
+}
+
+// age getter
+func (a animal) Age() int {
+	return a.age
+}
+
+// name getter
+func (a animal) Name() string {
+	return a.name
+}
 func main() {
 	fmt.Printf(morestrings.ReverseRunes("\nHello Go"))
+
+	myDog := animal{"Fred", 3}
+	fmt.Println(myDog.Age())
+	fmt.Println(myDog.Name())
 }
